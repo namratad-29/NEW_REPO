97cbfa5dc4f1ba9ab42b0085bba0b9d1cb5cefc7
namratad-29 <desainamrata29@gmail.com>
2026-07-04 15:25:23 +0530

    Add files via upload

---
+++ mobilebill.py
@@ -0,0 +1,9 @@
+mobile_name=input("Enter the mobile name: ")
+mobile_price=float(input("Enter the mobile price in INR: "))
+quantity=int(input("Enter the quantity: "))
+total_price = mobile_price * quantity
+print(f"MOBILE BILL")
+print(f"Mobile Name: {mobile_name}")
+print(f"Mobile Price: {mobile_price}")
+print(f"Quantity: {quantity}")
+print(f"Total Price: {total_price}")
